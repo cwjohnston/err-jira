5f989c2eacd017c2d7efada5762ecef80dcca0ed
rossnz <ross@trademe.co.nz>
2016-09-19 23:42:46 +1200

    Update jira.py
    Changed to use "new style" Python formatting. 
    
    Also changed output to use cards : http://errbot.io/en/latest/user_guide/plugin_development/messaging.html#cards

--- jira.py
+++ jira.py
@@ -1,16 +1,12 @@
 from errbot import BotPlugin
-import logging
-import re
-import requests
-
+import re, requests
 
 class Jira(BotPlugin):
-    """A plugin for interacting with Atlassian JIRA"""
-    min_err_version = '1.6.0'  # Optional, but recommended
-    max_err_version = '2.0.0'  # Optional, but recommended
+    """A plugin for retrieving ticket info from Atlassian JIRA"""
+    min_err_version = '1.6.0'
+    max_err_version = ''
 
     def get_configuration_template(self):
-        """Defines the configuration structure this plugin supports"""
         return {'URL': "http://jira.example.com",
                 'USERNAME': 'err',
                 'PASSWORD': 'secret',
@@ -18,36 +14,45 @@ class Jira(BotPlugin):
 
     def get_issue(self, issue_id):
         """Retrieves issue JSON from JIRA"""
-        response = requests.get(
-            self.config['URL']+'/rest/api/latest/issue/'+issue_id+'.json',
-            auth=(self.config['USERNAME'], self.config['PASSWORD']))
+        uri = self.config['URL']+'/rest/api/latest/issue/'+issue_id+'.json'
+        self.log.debug("Requesting URI: {}".format(uri))
+        response = requests.get(uri, auth=(self.config['USERNAME'], self.config['PASSWORD']))
         return response
 
-    def callback_message(self, conn, mess):
-        """A callback which responds to mention of JIRA issues"""
+    def callback_message(self, message):
         if self.config:
             matches = []
             regexes = []
+
             for project in self.config['PROJECTS']:
-                regexes.append(r'(%s\-[0-9]+)' % project)
+                self.log.debug("Appending project {} to search".format(project))
+                regexes.append(r"({}\-[0-9]+)".format(project))
             for regex in regexes:
-                matches.extend(re.findall(regex, mess.getBody(), flags=re.IGNORECASE))
+                self.log.debug("Searching for {} in {}".format(regex, message.body))
+                matches.extend(re.findall(regex, message.body, flags=re.IGNORECASE))
             if matches:
                 # set() gives us uniques, but does not preserve order.
+                self.log.debug("Match found!")
                 for match in set(matches):
                     issue_id = match
-                    logging.debug("[JIRA] matched issue_id: %s" % issue_id)
+                    self.log.debug("Matched issue_id: {}".format(issue_id))
                     issue_response = self.get_issue(issue_id)
                     if issue_response.status_code in (200,):
-                        logging.debug("[JIRA] retrieved issue data: %s" % issue_response)
+                        self.log.debug("Retrieved issue data: {}".format(issue_response))
+                        issue_id = issue_id.upper()
                         issue_summary = issue_response.json()['fields']['summary']
-                        html_message = "<html><body><a href=\"%s/browse/%s\">%s</a>: %s</body></html>" % (self.config['URL'], issue_id, issue_id, issue_summary,)
-                        self.send(mess.getFrom(), html_message, message_type=mess.getType())
+                        jira_link = "{}/browse/{}".format(self.config['URL'], issue_id)
+                        self.log.debug("Jira returned summary: {}".format(issue_summary))
+                        self.send_card(title = "JIRA case: {}".format(issue_id),
+                                       body = "JIRA: {}".format(issue_summary),
+                                       link = jira_link,
+                                       color = 'blue',
+                                       in_reply_to = message)
                     elif issue_response.status_code in (401,):
-                        self.send(mess.getFrom(), "Access Denied", message_type=mess.getType())
+                        self.send(message.frm, "Access Denied")
                     elif issue_response.status_code in (404,):
-                        self.send(mess.getFrom(), "Issue not found", message_type=mess.getType())
+                        self.send(message.frm, "Issue not found")
                     else:
-                        logging.error("[JIRA] encountered unknown response status code: %s" % issue_response.status_code)
-                        logging.error("[JIRA] response body: %s" % issue_response.json())
-                        self.send(mess.getFrom(), "Recieved an unexpected response, see logs for more detail", message_type=mess.getType())
+                        self.log.error("Encountered unknown response status code: {}".format(issue_response.status_code))
+                        self.log.error("Response body: {}".format(issue_response.json()))
+                        self.send(message.frm, "Recieved an unexpected response, see logs for more detail")
